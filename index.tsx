fe5a61df837a7551b85c91b0ca06c085331ac2e4
bdowneer191 <bdowneer.bd1@yahoo.com>
2025-07-21 01:18:47 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ClerkProvider } from '@clerk/clerk-react';
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
+import { AuthProvider } from './hooks/useAuth.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -18,8 +11,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <AuthProvider>
       <App />
-    </ClerkProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
