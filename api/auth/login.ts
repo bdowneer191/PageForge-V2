4ca05ebc3b587ade16942f53c9da945938181be0
bdowneer191 <bdowneer.bd1@yahoo.com>
2025-07-21 01:35:30 +0600

    Delete api/auth directory

--- api/auth/login.ts
+++
@@ -1,6 +0,0 @@
-
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-    res.status(404).json({ error: 'Not Found' });
-}
